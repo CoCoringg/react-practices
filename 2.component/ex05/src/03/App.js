65e6376cbea5618377ad5bdbdff92cf95036ea4a
chaewon <rlacodnjs98@naver.com>
2022-07-25 19:33:59 +0900

    Clock commit

--- 2.component/ex05/src/03/App.js
+++ 2.component/ex05/src/03/App.js
@@ -5,11 +5,12 @@ import Clock from './Clock';
 export default class App extends Component {
     constructor(props) {
         super(props);
+        const today = new Date();
         this.state = {
-            hours: 12,
-            minutes: 12,
-            seconds: 30,
-            session: 'am'
+            hours: today.getHours() % 12 ? today.getHours() % 12 : 12,
+            minutes: today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes(),
+            seconds: today.getSeconds() < 10 ? '0' + today.getSeconds() : today.getSeconds(),
+            session: today.getHours() >= 12 ? 'pm' : 'am'
         }
     }
 
@@ -17,8 +18,25 @@ export default class App extends Component {
         return (
             <div className='clock-display'>
                 <h2>ex05 - Class Component LifeCycle Practice</h2>
-                    <Clock hours={this.state.hours} minutes={12} seconds={30} session={'pm'}/>
+                    <Clock hours={this.state.hours} minutes={this.state.minutes} seconds={this.state.seconds} session={this.state.session}/>
             </div>
         );
     }
+
+    componentDidMount() {
+        setInterval(() => {
+            const today = new Date();
+            this.setState({
+                hours: today.getHours() % 12 ? today.getHours() % 12 : 12,
+                minutes: today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes(),
+                seconds: today.getSeconds() < 10 ? '0' + today.getSeconds() : today.getSeconds(),
+                session: today.getHours() >= 12 ? 'pm' : 'am'
+            })
+        }, 1000)
+    }
+        
 }
+
+    
+
+
--- 2.component/ex05/src/04/App.js
+++ 2.component/ex05/src/04/App.js
@@ -3,13 +3,35 @@ import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default function App() {
+    const today = new Date();
 
+    const [state, setState] = useState({
+        hours: today.getHours() % 12 ? today.getHours() % 12 : 12,
+        minutes: today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes(),
+        seconds: today.getSeconds() < 10 ? '0' + today.getSeconds() : today.getSeconds(),
+        session: today.getHours() >= 12 ? 'pm' : 'am'
+    });
+
+
+    useEffect(() => {
+        const intervalID = setInterval(() =>
+        setState({
+            hours: today.getHours() % 12 ? today.getHours() % 12 : 12,
+            minutes: today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes(),
+            seconds: today.getSeconds() < 10 ? '0' + today.getSeconds() : today.getSeconds(),
+            session: today.getHours() >= 12 ? 'pm' : 'am'
+          }), 1000);
+        return () => {
+          clearInterval(intervalID);
+        }
+      }, [state])
 
     return (
             <Clock
                 message={'ex05: useEffect Hook example'}
-                hours={hours}
-                minutes={minutes}
-                seconds={seconds}/>
+                hours={state.hours}
+                minutes={state.minutes}
+                seconds={state.seconds}
+                session={state.session}/>
     );
 }
